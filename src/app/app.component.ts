3b324d3842dc04b0802f617a0b5bd6ca4393b5d3
KaiqueFLH <kaique_homem@estudante.sesisenai.org.br>
2023-04-27 22:00:18 -0300

    sla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+interface Tarefas{
+  nome:String;
+  categoria:String;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todo-app';
+
+  tarefa:Tarefas={
+    nome:'',
+    categoria:''
+  }
+
+  usuarios:Tarefas[] = [];
+
+  cadastrarUsuario():void{
+    const usuario:Tarefas={
+      nome: this.tarefa.nome,
+      categoria: this.tarefa.categoria
+    }
+   
+    this.usuarios.push(usuario);
+    console.log(this.usuarios)
+  }
+
+
+  removerTarefa():void{
+    
+  }
 }
