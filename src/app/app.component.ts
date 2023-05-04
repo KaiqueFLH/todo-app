a56249f2ff0a4645197abf9878b1a484e267e3de
KaiqueFLH <Kaique_estudante.sesisenai.org.br>
2023-05-04 16:43:02 -0300

    Resta local Sorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { stringify } from 'querystring';
 
 interface Tarefas{
   nome:String;
   categoria:String;
   descricao:String;
+  id:number;
 }
 
 @Component({
@@ -14,31 +16,41 @@ interface Tarefas{
 
 export class AppComponent {
 
-  tarefa:Tarefas={
-    nome:'',
-    categoria:'',
-    descricao:''
+  ngOnInit():void{
+    this.tarefas = window.JSON.parse(localStorage.getItem("Lista de Tarefas"))
   }
 
   tarefas:Tarefas[] = [];
 
-  cadastrarUsuario():void{
-    const usuario:Tarefas={
-      nome: this.tarefa.nome,
-      categoria: this.tarefa.categoria,
-      descricao:this.tarefa.descricao
-    }
+  tarefa:Tarefas={
+    nome:null,
+    categoria:null,
+    descricao:null,
+    id:0
+  }
 
-    this.tarefa.nome=null;
-    this.tarefa.descricao=null;
-    this.tarefa.categoria=null;
-   
-    this.tarefas.push(usuario);
-    console.log(this.tarefas)
+  cadastrarUsuario():void{
+    if(this.tarefa.nome!=null && this.tarefa.categoria!=null ){
+      const usuario:Tarefas={
+        nome: this.tarefa.nome,
+        categoria: this.tarefa.categoria,
+        descricao:this.tarefa.descricao,
+        id: this.tarefa.id++
+      }
+      this.tarefas.push(usuario);
+      localStorage.setItem("Lista de Tarefas",JSON.stringify(this.tarefas))
+    }
+    
+      this.tarefa.nome=null;
+      this.tarefa.descricao=null;
+      this.tarefa.categoria=null;
+    
+    
   }
 
 
   removerTarefa(indice):void{
     this.tarefas.splice(indice,1)
+    localStorage.setItem("Lista de Tarefas",JSON.stringify(this.tarefas))
   }
 }
