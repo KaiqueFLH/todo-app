358c21c57b9b23e0413c9c5b4ff3688410907f2a
KAIQUE FERNANDES LIMA HOMEM <kaique_homem@estudante.sesisenai.org.br>
2023-06-19 21:50:38 -0300

    Parte de Propriedades quase finalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { CadastroTarefaComponent } from 'src/cadastroTarefa/cadastroTarefa.compo
 import { AppRoutingModule } from './app.routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/cadastroCategoria/cadastroCategoria.component.ts
+++ src/cadastroCategoria/cadastroCategoria.component.ts
@@ -1,13 +1,13 @@
 import { Component } from "@angular/core";
-import { splitNamespace } from "@angular/core/src/view/util";
 
-interface Categoria{
-  nomeCategoria:String;
-  cor:String;
+interface Propriedade{
+  nome: String,
+  tipo:String,
+  conteudo:String[] | number | String;
 }
 
 @Component({
-  selector: 'categoria-root',
+  selector: 'propriedade-root',
   templateUrl: '../cadastroCategoria/cadastroCategoria.component.html',
   styleUrls: ['../cadastroCategoria/cadastroCategoria.component.css']
 })
@@ -15,17 +15,17 @@ interface Categoria{
 export class CadastroCategoriaComponent {
   
   ngOnInit(): void {
-    if (localStorage.getItem('listaCategorias') != null) {
-      this.listaCategorias = JSON.parse(localStorage.getItem('listaCategorias'));
+    if (localStorage.getItem('listaProps') != null) {
+      this.listaProps = JSON.parse(localStorage.getItem('listaProps'));
     }
-
   }
-  categoriaExistente:Categoria;
-  nomeCategoria:String="";
-  cor:String="#ffffff";
-  listaCategorias: Categoria[] = [];
-  
-  
+
+  nome: String;
+  tipo:String;
+  conteudo:String[] | number | String;
+  listaProps: Propriedade[] = [];
+  listaTipos: String[] = ["Texto", "Número", "Seleção"]
+
 
   teste = false;
 
@@ -41,16 +41,16 @@ export class CadastroCategoriaComponent {
 
 
 
-  verificarIgualdade(): boolean {
+  // verificarIgualdade(): boolean {
 
-    for (const i of this.listaCategorias) {
-      if (i.nomeCategoria === this.nomeCategoria) {
-        return false;
-      }
-    }
+  //   for (const i of this.listaProps) {
+  //     if (i.nome === this.listaProps) {
+  //       return false;
+  //     }
+  //   }
 
-    return true;
-  }
+  //   return true;
+  // }
 
   contraste(cor: string): string {
     const r = parseInt(String(cor).substr(1, 2), 16);
@@ -61,48 +61,81 @@ export class CadastroCategoriaComponent {
   }
 
 
-  cadastrarCategoria(): void {
-    console.log(this.nomeCategoria);
-
-    const categoria: Categoria = {
-      nomeCategoria: this.nomeCategoria,
-      cor: this.cor
+  cadastrarPropriedade():void{
+    const prop:Propriedade = {
+      nome:this.nome,
+      tipo:this.tipo,
+      conteudo:this.conteudo
     }
 
-    if (this.nomeCategoria != "") {
-      
-      if (this.verificarIgualdade()) {
-        this.listaCategorias.push(categoria);
-        
-        localStorage.setItem('listaCategorias', JSON.stringify(this.listaCategorias));
-      }
-      else {
-        alert("A Categoria que você está tentando cadastrar, já existe!")
-      }
-    }
-
-    this.nomeCategoria = '';
-
-  }
-
-  removerCategoria(indice): void {
-    let listaTarefas = []
-    if (localStorage.getItem('Lista de Tarefas') != null) {
-      listaTarefas = JSON.parse(localStorage.getItem('Lista de Tarefas'));
-    }
+    this.listaProps.push(prop);
+    console.log(prop.tipo);
     
-    for (let tarefa of listaTarefas) {
-      console.log('a');
-      if (tarefa.categoria == this.listaCategorias[indice]) {
 
-        listaTarefas.splice(listaTarefas.indexOf(tarefa), listaTarefas.length)
-      }
-    }
-
-    localStorage.setItem('Lista de Tarefas', JSON.stringify(listaTarefas))
-
-    this.listaCategorias.splice(indice, 1)
-    localStorage.setItem('listaCategorias', JSON.stringify(this.listaCategorias));
+    this.localStorage();
 
   }
+
+  removerPropriedade(indice):void{
+    this.listaProps.splice(indice,1);
+    this.localStorage();
+  }
+
+  localStorage():void{
+    return localStorage.setItem("listaProps", JSON.stringify(this.listaProps));
+  }
+
+
+
+
+
+
+
+
+
+
+  // cadastrarCategoria(): void {
+  //   console.log(this.nomeCategoria);
+
+  //   const categoria: Categoria = {
+  //     nomeCategoria: this.nomeCategoria,
+  //     cor: this.cor
+  //   }
+
+  //   if (this.nomeCategoria != "") {
+      
+  //     if (this.verificarIgualdade()) {
+  //       this.listaCategorias.push(categoria);
+        
+  //       localStorage.setItem('listaCategorias', JSON.stringify(this.listaCategorias));
+  //     }
+  //     else {
+  //       alert("A Categoria que você está tentando cadastrar, já existe!")
+  //     }
+  //   }
+
+  //   this.nomeCategoria = '';
+
+  // }
+
+  // removerCategoria(indice): void {
+  //   let listaTarefas = []
+  //   if (localStorage.getItem('Lista de Tarefas') != null) {
+  //     listaTarefas = JSON.parse(localStorage.getItem('Lista de Tarefas'));
+  //   }
+    
+  //   for (let tarefa of listaTarefas) {
+  //     console.log('a');
+  //     if (tarefa.categoria == this.listaCategorias[indice]) {
+
+  //       listaTarefas.splice(listaTarefas.indexOf(tarefa), listaTarefas.length)
+  //     }
+  //   }
+
+  //   localStorage.setItem('Lista de Tarefas', JSON.stringify(listaTarefas))
+
+  //   this.listaCategorias.splice(indice, 1)
+  //   localStorage.setItem('listaCategorias', JSON.stringify(this.listaCategorias));
+
+  // }
 }
